95cbb851f2ec9f96dcfb44bf267ed94439007c2b
Daffa954 <124135109+Daffa954@users.noreply.github.com>
2026-06-08 06:57:35 +0700

    change register

--- Frontend/frontend-react/src/App.tsx
+++ Frontend/frontend-react/src/App.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
-import VerifyPage from "./pages/VerifyPage";
 import LandingPage from "./pages/LandingPage";
 import ProfilePage from "./pages/ProfilePage";
-import OnboardingPage from "./pages/OnBoardingPage";
 import MainLayout from "./layouts/MainLayout";
 
 // Import Pages (Pastikan Anda membuat file-file ini)
@@ -13,18 +11,17 @@ import IssueDocument from "./pages/IssueDocument";
 import PatientDashboard from "./pages/PatientDashboard";
 import ScannerPage from "./pages/ScannerPage";
 import VerifyResultPage from "./pages/VerifyResultPage";
+import OnboardingPage from "./pages/OnboardingPage";
 
 function App() {
   return (
     <Routes>
       {/* A. Halaman Publik */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<OnboardingPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/verify" element={<VerifyPage />} />
+      <Route path="/verify" element={<VerifyResultPage />} />
       <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/onboarding" element={<MainLayout><OnboardingPage /></MainLayout>} />
 
       {/* B. Portal Admin */}
       <Route path="/admin/dashboard" element={<MainLayout><AdminDashboard /></MainLayout>} />
